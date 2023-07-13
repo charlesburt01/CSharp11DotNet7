6018b8e8edc167439f4ca21c0024dca5744cf63e
Charles Burt <ctf_exe@hotmail.com>
2023-07-13 13:43:25 +0100

    Wrote a generic method, fix bugs

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,15 +1,20 @@
 ﻿//Generics
-int[] myInt = 0,1,2;
-string[] myString = "hello", "there", "mate";
-double[] myDouble = 0.1m, 0.2m, 0.3m;
+//We can use one function below, instead of writing three!
+int[] myInt = { 0, 1, 2 };
+string[] myString = { "hello", "there", "mate" };
+double[] myDouble = { 0.1, 0.2, 0.3 };
 
-void PrintLoop<Thing>(Thing thing)
+void PrintLoop<Thing>(Thing[] thing)
 {
     foreach (Thing t in thing)
     {
         Console.WriteLine(t);
     }
 }
+
+PrintLoop(myInt);
+PrintLoop(myString);
+PrintLoop(myDouble);
 /*
 Person harry = new()
 {
